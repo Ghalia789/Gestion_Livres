757831be4262d26cb16d8e744a279eb020b76975
Ghalia789 <ghaliabellalouna@gmail.com>
2023-11-16 10:58:40 +0100

    modified:   Front-angular-gestion-livres-main - Copie/src/app/add-livre/add-livre.component.ts

--- Front-angular-gestion-livres-main - Copie/src/app/add-livre/add-livre.component.ts
+++ Front-angular-gestion-livres-main - Copie/src/app/add-livre/add-livre.component.ts
@@ -4,7 +4,7 @@ import { Livre } from '../model/livre.model';
 import { LivreService } from '../service/livre.service';
 import { Genre } from '../model/genre.model';
 import { GenreWrapper } from '../model/GenreWrapped';
-
+import { Image } from '../model/image.model';
 @Component({
   selector: 'app-add-livre',
   templateUrl: './add-livre.component.html',
@@ -30,14 +30,28 @@ export class AddLivreComponent implements OnInit {
 
 
 
-  addLivre() {
+  /*addLivre() {
     this.newLivre.genre = this.genres.find(gen => gen.idGen == this.newIdGen)!;
     this.livreService.ajouterLivre(this.newLivre)
       .subscribe(liv => {
         console.log(liv);
         this.router.navigate(['livres']);
       });
+  }*/
+  addLivre() {
+    this.livreService.uploadImage(this.uploadedImage, this.uploadedImage.name)
+      .subscribe((img: Image) => {
+        this.newLivre.image = img;
+        this.newLivre.genre = this.genres.find(gen => gen.idGen == this.newIdGen)!;
+
+        this.livreService.ajouterLivre(this.newLivre)
+          .subscribe(liv => {
+            console.log(liv);
+            this.router.navigate(['livres']);
+          });
+      });
   }
+
   uploadedImage!: File;
   imagePath: any;
   onImageUpload(event: any) {
